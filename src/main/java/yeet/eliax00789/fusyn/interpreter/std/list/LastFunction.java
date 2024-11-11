dd62aa878a6cbd1f14a3e7716179870b1abb346d
Eliax00789 <el-p@web.de>
2024-11-12 00:18:17 +0100

    Added list::first and list::last functions

---
+++ src/main/java/yeet/eliax00789/fusyn/interpreter/std/list/LastFunction.java
@@ -0,0 +1,35 @@
+package yeet.eliax00789.fusyn.interpreter.std.list;
+
+import org.jetbrains.annotations.NotNull;
+import yeet.eliax00789.fusyn.interpreter.Interpreter;
+import yeet.eliax00789.fusyn.interpreter.NativeFunction;
+
+import java.util.List;
+
+@SuppressWarnings("unused")
+public class LastFunction implements NativeFunction {
+    @Override
+    public String getName() {
+        return "list::last";
+    }
+
+    @Override
+    public List<String> getArgumentTypes() {
+        return List.of("List");
+    }
+
+    @Override
+    public List<String> getArgumentNames() {
+        return List.of("list");
+    }
+
+    @Override
+    public String getReturnType() {
+        return "Any";
+    }
+
+    @Override
+    public Object execute(Interpreter interpreter, @NotNull List<?> arguments, int position, @NotNull List<Integer> argumentPositions) {
+        return ((List<?>) arguments.getFirst()).getLast();
+    }
+}
